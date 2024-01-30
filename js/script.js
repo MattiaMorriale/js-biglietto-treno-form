afb404b269bac692f859fb0d6350d5368b33c28d
mattia_morriale <mattia.morriale1@gmail.com>
2024-01-30 18:03:48 +0100

    fine dell'esercizio

--- js/script.js
+++ js/script.js
@@ -4,30 +4,39 @@ const kmkElement = document.querySelector("#kmk");
 
 const buttonElement = document.querySelector("#button")
 
+const ageElement = document.querySelector("#age");
+
+const agekElement = document.querySelector("#agek");
+
+const nameElement = document.querySelector("#name")
+
+const namekElement = document.querySelector("#namek")
+
 buttonElement.addEventListener("click",
     function(){
-        kmElement.value = kmkElement.value;
+        let nameValue=nameElement.value;
+        namekElement.innerHTML=nameValue;
 
-        console.log("kmkElement")
+        let valore_impuit_mk=kmElement.value;
+        kmkElement.innerHTML=valore_impuit_mk;
 
-        document.getElementById("kmkElement").innerHTML = kmkElement;
+        let ageValue= ageElement.value;
+        agekElement.innerHTML=ageValue;
+        
+        const prezzo = 0.21;
+
+        const pfinale = prezzo * valore_impuit_mk;
+
+        document.getElementById("pfinale").innerHTML = pfinale.toFixed(2);
+
+        if ( ageValue < 18 ) {
+        
+            document.getElementById("pfinale").innerHTML = pfinale - ( pfinale * 20 / 100 ).toFixed(2);
+        
+        } else if ( ageValue > 64 ) {
+        
+            document.getElementById("pfinale").innerHTML = pfinale - ( pfinale * 40 / 100 ).toFixed(2);
+        
+        }
     }
 )
-
-const age = document.querySelector("#age");
-
-const prezzo = 0.21;
-
-const pfinale = prezzo * km;
-
-document.getElementById("pfinale").innerHTML = pfinale.toFixed(2);
-
-if ( age < 18 ) {
-
-    document.getElementById("pfinale").innerHTML = pfinale - ( pfinale * 20 / 100 ).toFixed(2);
-
-} else if ( age > 64 ) {
-
-    document.getElementById("pfinale").innerHTML = pfinale - ( pfinale * 40 / 100 ).toFixed(2);
-
-}
